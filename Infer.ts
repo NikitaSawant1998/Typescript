c031b1e371e9288fe1e68c66a7fc2e8b4eaa69ae
Nikita S <nikitasawant5193@gmail.com>
2025-08-01 18:02:17 +0530

    infer in ts

---
+++ Infer.ts
@@ -0,0 +1,5 @@
+//infer mean one type of predict
+//infering datatype
+let n1:number=5//explicit infer
+let n2=8 //implicit infer
+console.log(n1+n2)
---
+++ infer.js
@@ -0,0 +1,5 @@
+//infer mean one type of predict
+//infering datatype
+var n1 = 5; //explicit infer
+var n2 = 8; //implicit infer
+console.log(n1 + n2);
