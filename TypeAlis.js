6b08dadd2604dcb17853cce98f9ab9318dd5b239
Nikita S <nikitasawant5193@gmail.com>
2025-08-01 18:04:17 +0530

    Type in ts

---
+++ TypeAlis.js
@@ -0,0 +1,2 @@
+var user1 = { name: "Alice", age: 22 };
+console.log(user1);
---
+++ TypeAlis.ts
@@ -0,0 +1,4 @@
+//Type Aliases Reusable types for objects:
+type User = { name: string; age: number };  
+let user1: User = { name: "Alice", age: 22 };
+console.log(user1)
